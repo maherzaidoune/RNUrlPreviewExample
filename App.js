7f49d5190a56bb7950fb30aa2226cedd771817d2
Maher Zaidoune <maherzaidoune@gmail.com>
2019-01-13 10:11:34 +0100

    screenshots

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <RNUrlPreview text={"https://www.youtube.com/watch?v=Kmiw4FYTg2U"} />
+        <RNUrlPreview text={"Myrath dance https://www.youtube.com/watch?v=b2rKORvyOcM"} />
       </View>
     );
   }
