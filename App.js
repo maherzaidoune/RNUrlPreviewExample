cae39eae43580aa367a265e82dfdc02d20be6fbc
maherzaidoune <maherzaidoune@gmail.com>
2020-07-30 21:04:15 +0100

    bump version react-native-url-preview 1.1.9

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
 import RNUrlPreview from "react-native-url-preview";
 export default class App extends Component<Props> {
+
+  renderItem = ({ item }) => {
+    return(
+      <RNUrlPreview text={item} />
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
-        <RNUrlPreview text={"Myrath dance https://medium.com/s/user-friendly/emulation-is-not-a-product-strategy-cfecdbffce96"} />
+        <FlatList
+        style={{
+          width: '100%'
+        }}
+          data={[
+            "https://reactnativeforyou.com/how-to-show-url-previews-in-react-native/",
+            "Myrath dance https://medium.com/s/user-friendly/emulation-is-not-a-product-strategy-cfecdbffce96",
+          ]}
+          renderItem={this.renderItem}
+          ItemSeparatorComponent={
+            () => (
+              <View
+                style={{
+                  height: 10
+                }}
+              />
+            )
+          }
+        />
       </View>
     );
   }
